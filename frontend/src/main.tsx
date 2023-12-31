9ec6812c2f98bd57acf770d73567a35f62c9b259
Rishabh Jain <rishabh.1514@gmail.com>
2023-12-31 19:21:54 +0530

    Changed axis default url

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
-axios.defaults.baseURL = "https://mern-gpt-backend.vercel.app/api/v1/"; //change later
+axios.defaults.baseURL = "https://mern-gpt-backend.onrender.com/api/v1/"; //change later
 axios.defaults.withCredentials = true;
 
 const theme = createTheme({
